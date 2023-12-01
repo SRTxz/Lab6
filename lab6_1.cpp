9abd6a780ef86aa025fe5d433eb1a93f0f20de63
Saran Maihom <sarantoey8@gmail.com>
2023-12-01 14:35:54 +0700

    555

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Enter an integer: ";
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
+    cout << "555";
     return 0;
 }
